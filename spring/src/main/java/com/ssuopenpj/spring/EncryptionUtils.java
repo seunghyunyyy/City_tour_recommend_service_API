8726b49ffcd17de256a6233eddc33102f78f4418
seunghyunyyy <seunghyunyyy@kakao.com>
2023-11-29 01:34:45 +0900

    Remove Annotation

--- spring/src/main/java/com/ssuopenpj/spring/EncryptionUtils.java
+++ spring/src/main/java/com/ssuopenpj/spring/EncryptionUtils.java
@@ -34,34 +34,4 @@ public class EncryptionUtils {
 
         return result;
     }
-    /*public static String getEncrypt(String s, String messageDigest) {
-        try {
-            MessageDigest md = MessageDigest.getInstance(messageDigest);
-            byte[] passBytes = s.getBytes();
-            md.reset();
-            byte[] digested = md.digest(passBytes);
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < digested.length; i++) sb.append(Integer.toString((digested[i]&0xff) + 0x100, 16).substring(1));
-            return sb.toString();
-        } catch (Exception e) {
-            return s;
-        }
-    }
-    public String getSalt() {
-        //1. Random, salt 생성
-        SecureRandom sr = new SecureRandom();
-        byte[] salt = new byte[20];
-
-        //2. 난수 생성
-        sr.nextBytes(salt);
-
-        //3. byte To String (10진수 문자열로 변경)
-        StringBuffer sb = new StringBuffer();
-        for(byte b : salt) {
-            sb.append(String.format("%02x", b));
-        }
-
-        return sb.toString();
-    }
-*/
 }
