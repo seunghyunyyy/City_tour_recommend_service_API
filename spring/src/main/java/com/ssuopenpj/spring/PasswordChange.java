70774ac596fa0cd86fcad2be5bb426c3ba48402e
seunghyunyyy <seunghyunyyy@kakao.com>
2023-11-29 00:46:49 +0900

    PasswordChange Object setting

---
+++ spring/src/main/java/com/ssuopenpj/spring/PasswordChange.java
@@ -0,0 +1,11 @@
+package com.ssuopenpj.spring;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PasswordChange {
+    private String pw1;
+    private String pw2;
+}
